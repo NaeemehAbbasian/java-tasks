66555c1f9a53ea095fd3a09be9b50918d7bc5d8f
naeemeh <refreshsho@gmail.com>
2025-08-20 10:49:54 +0330

    practice 2

--- js/app.js
+++ js/app.js
@@ -1,6 +1,25 @@
+//practice1
 const numbers =  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const evenNumbers = numbers.filter (number => {
   return number %2 === 0 ;
 });
 console.log(evenNumbers);
 
+//practice2
+function flattenArrayWithForEach(arr) {
+  let flattened = [];
+   arr.forEach(element => {
+    if (Array.isArray(element)) {
+      flattened.push(...flattenArrayWithForEach(element));
+    } 
+    else {
+      flattened.push(element);
+       
+    }   
+  }); 
+  return flattened; 
+}
+const nestedArray = [1, [2, [3, [4]], 5]];
+const flattenedArray = flattenArrayWithForEach(nestedArray);
+
+console.log(flattenedArray);
