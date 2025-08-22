344cf5a59bb41a97dd1b1e912f1efd0c031d0303
naeemeh <refreshsho@gmail.com>
2025-08-22 12:30:01 +0330

    practice 10

--- js/app.js
+++ js/app.js
@@ -85,7 +85,6 @@ const element = ["a", "b", "a", "d", "c", "d", "a", "c", "b", "b", "a"];
 const result = countFrequencies(element);
 console.log(result);
 
-
 //practice 7
 const oldNumbers = [
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
@@ -93,9 +92,9 @@ const oldNumbers = [
 ];
 const newArray = oldNumbers.map((number) => {
   if (number <= 12) {
-    return `${ number } AM`;
+    return `${number} AM`;
   } else {
-    const newNumber = number -  12 ; 
+    const newNumber = number - 12;
     return `${newNumber} PM`;
   }
 });
@@ -105,29 +104,46 @@ console.log(newArray);
 const num = 15;
 const min = 10;
 const max = 20;
-function isWithinRange (num, min, max) {
-  if ( num >= min && num<= max) { return true;
-    } else {return false;}
+function isWithinRange(num, min, max) {
+  if (num >= min && num <= max) {
+    return true;
+  } else {
+    return false;
+  }
 }
-console.log(isWithinRange (num, min, max));
-
+console.log(isWithinRange(num, min, max));
 
 //practice 9
 const weight = 80;
-const height = 1.90;
+const height = 1.9;
 
-function catagorizeWeight (weight, height) {
+function catagorizeWeight(weight, height) {
   const result = weight / (height * height);
-  if ( result < 18.5 ) { return 'Underweight';} 
-  else if ( result >= 18.5 && result <= 24.9) { return 'NormalWeight';} 
-  else if ( result >= 25 && result <= 29.9) { return 'Overweight';}
-    else {return 'Obesity';}
-}  
-  
-console.log(catagorizeWeight (weight, height));
+  if (result < 18.5) {
+    return "Underweight";
+  } else if (result >= 18.5 && result <= 24.9) {
+    return "NormalWeight";
+  } else if (result >= 25 && result <= 29.9) {
+    return "Overweight";
+  } else {
+    return "Obesity";
+  }
+}
 
+console.log(catagorizeWeight(weight, height));
 
+//practice 10
+const text = "Hello World! What a beautiful day.";
 
+function hasSpecificLetters(text) {
+  const letters = ["a", "e", "i", "o", "u"];
+  const lowerCaseText = text.toLowerCase();
 
-
-
+  for (const letter of letters) {
+    if (lowerCaseText.includes(letter)) {
+      return true;
+    }
+  }
+  return false;
+}
+console.log( hasSpecificLetters(text));
